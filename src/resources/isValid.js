18865fc7ad7ee3aa8ce391e09c5e21871b872202
Rogerio Ushiro <rogerio.ushiro@gmail.com>
2023-10-06 18:46:54 -0300

    added more criteria to validate

--- src/resources/isValid.js
+++ src/resources/isValid.js
@@ -1,26 +1,32 @@
-const contains = (text, array) => {
+const mustContains = (text, array) => {
+  if (array.length == 0) return true;
   var validWords = 0;
   for (let index = 0; index < array.length; index++) {
     const element = array[index];
-    if (text.toLowerCase().includes(element.toLowerCase())) validWords++;
+    if (text.description.toLowerCase().includes(element.toLowerCase())) validWords++;
   }
   return validWords === array.length;
 };
 
-const notContains = (text, array) => {
+const mustNotContains = (text, array) => {
+  if (array.length == 0) return true;
   var result = true;
   for (let index = 0; index < array.length; index++) {
     const element = array[index];
-    if (text.toLowerCase().includes(element.toLowerCase())) result = false;
+    if (text.description.toLowerCase().includes(element.toLowerCase())) result = false;
   }
   return result;
 };
-const isValid = (result, mustHave, mustNotHave) => {
+
+const isValid = (result, data) => {
+  const mustHaveEnglish = data.english ? result.english : true;
+  const mustBeRemote = data.remote ? result.remote : true;
   return (
-    result.english &&
-    result.remote &&
-    contains(result, mustHave) &&
-    notContains(result, mustNotHave)
+    mustHaveEnglish &&
+    mustBeRemote &&
+    mustContains(result, data.mustHave) &&
+    mustNotContains(result, data.mustNotHave)
   );
 };
+
 module.exports = isValid;
